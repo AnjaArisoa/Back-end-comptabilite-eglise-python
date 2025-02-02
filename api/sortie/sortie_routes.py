5cf76d5213df4f9db237e139ee4acec1e3648a76
AnjaArisoa <arisoaanja@gmail.com>
2025-02-02 18:26:28 +0300

    feat-detail_entree

--- api/sortie/sortie_routes.py
+++ api/sortie/sortie_routes.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends,status,HTTPException
 from sqlalchemy.orm import Session
 from fastapi import HTTPException
 from api.sortie.sortie_model import sortie_create
-from api.sortie.sortie_services import create_sortie,get_sortie
+from api.sortie.sortie_services import create_sortie,get_sortie,get_detail_sortie
 
 
 from core.database import get_session
@@ -20,5 +20,12 @@ def sortie_create(create_data:sortie_create,session : Session=Depends(get_sessio
 def get_sorties(session : Session=Depends(get_session)):
     try:
         return get_sortie(session)
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=str(e))
+    
+@router.get("/get_detail_sortie/{idSortie}")
+def get_detail_sorties(idSortie:str,session : Session=Depends(get_session)):
+    try:
+        return get_detail_sortie(idSortie,session)
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=str(e))
