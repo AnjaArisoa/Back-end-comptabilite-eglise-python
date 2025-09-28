585672afe0ac2b8652c7ca8e4d8786ed11512413
AnjaArisoa <119120342+AnjaArisoa@users.noreply.github.com>
2025-09-28 14:48:57 +0300

    vcccc

--- core/database.py
+++ core/database.py
@@ -1,8 +1,11 @@
 from sqlmodel import SQLModel, create_engine, Session
 from core.config import DATABASE_URL
 
-
+# Créer l'engine
 engine = create_engine(DATABASE_URL)
+
+# Définir Base pour Alembic
+Base = SQLModel.metadata  # Alembic a besoin de target_metadata
 
 def init_db():
     SQLModel.metadata.create_all(engine)
